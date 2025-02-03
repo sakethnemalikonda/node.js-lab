3b76fb296d01ca41d803ddfacf539f42d0cb8428
Nemalikonda Saketh <92433733+sakethnemalikonda@users.noreply.github.com>
2025-02-03 15:50:58 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,44 @@
+function validate(){
+    var fn=frm.fname.value;
+    localStorage.setItem("firstname",fn)
+    for(x in fn){
+        ch=fn.charCodeAt(x);
+        if(ch<65 || ch>90 && ch<97 || ch>122){
+            alert("Invalid firstname");
+            return false;
+        }
+    }
+    var In=frm.lname.value;
+    localStorage.setItem("lastname",ln)
+    for(y in fn){
+        ch=fn.charCodeAt(y);
+        if(ch<65 || ch>90 && ch<97 || ch>122){
+            alert("Invalid lastname");
+            return false;
+        }
+    }
+    var phn=frm.phone.value;
+    localStorage.setItem("phone",phn)
+    var lenp=phn.length;
+    if(lenp!==10){
+        alert("Phone number should be 10 digits");
+        return false;
+    }
+    var pwd1=frm.pwd.value;
+    localStorage.setItem("password",pwd1)
+    var pwd1=pwd1.length;
+    if(pwd1%2==1){
+        alert("Password should contain even number of characters");
+        return false;
+    }
+    if(pwd1>8){
+        alert("Password should not exceed 8 digits");
+        return false;
+    }
+    var reg=/^\w+([-+,']\w+)*@\w+([-+,']\w+)*\.\w+([-+,']\w+)*$/;
+    var mail=frm.mailid.value;
+    localStorage.setItem("email",mail)
+    if(reg.test(mail)){
+        
+    }
+}
